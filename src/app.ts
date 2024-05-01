7b8cef4317fe8917129cd0985ddfad77f9345743
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-05-02 08:48:14 +0900

    setup api server

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express,{Request,Response,NextFunction} from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response,next:NextFunction) => {
+    res.send('Welcome to the server!');
+});
+
+app.get('/api', (req: Request, res: Response,next:NextFunction) => {
+    res.send('Welcome to the API!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
